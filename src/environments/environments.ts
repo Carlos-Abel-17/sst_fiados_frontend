417b80059fc5f6554658dd2ca1a729b39aaf3e2f
Carlos Abel <135867557+Carlos-Abel-17@users.noreply.github.com>
2026-06-29 21:35:07 -0500

    cambio de direccion

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sstfiadosbackend-production.up.railway.app/api',
+  apiUrl: 'https://sstfiadosbackend-production.up.railway.app/api/',
 };
